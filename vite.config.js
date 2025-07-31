f83c6f91651427c5d5c9dff2bb28bcfba8027378
Rizqi Arief Wicaksono <rizqiariefwicaksono@Rizqis-MacBook-Pro.local>
2025-07-31 14:43:23 +0700

    perbarui

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import vue from '@vitejs/plugin-vue';
 
 export default defineConfig({
     build: {
-        outDir: 'dist', 
+        outDir: 'public/build', 
     },
     plugins: [
         laravel({
