123c1169abf3c37b0c40062368fe1ce49c8deaf9
Rizqi Arief Wicaksono <rizqiariefwicaksono@Rizqis-MacBook-Pro.local>
2025-07-31 16:58:52 +0700

    ai

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,3 @@
 <?php
 
-use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\ImageGeneratorController;
-
-Route::post('/generate-image', [ImageGeneratorController::class, 'generate']);
+// Kosongkan atau tambahkan komentar saja
